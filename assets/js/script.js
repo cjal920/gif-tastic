cd56b053c9000866d10137468eabf4c0ed52a642
Colin Larkin <colinlarkin@Colins-MacBook-Air.local>
2018-06-16 21:45:30 -0400

    first code commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var gifFather = ["Don Vito","Michael","Kay Adams","Sonny","Fredo","Connie","Tom Hagan","Clemenza","Tessio","Virgil Solozzo",
+                "Jack Woltz","Apollonia","Frank Pentangeli","Hyman Roth","Don Fanucci","Vincenzo Corleone","Joey Zasa"];
+
+
+
+
+var queryURL;
+
+$.ajax();    {
+
+
+
+}
