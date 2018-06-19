021a2e3653471ef54d4ea9660c272b1682ce3cc6
Colin Larkin <colinlarkin@Colins-MacBook-Air.local>
2018-06-18 20:37:40 -0400

    second code update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,100 @@
-var gifFather = ["Don Vito","Michael","Kay Adams","Sonny","Fredo","Connie","Tom Hagan","Clemenza","Tessio","Virgil Solozzo",
-              "Jack Woltz","Apollonia","Al Neri","Willy Cicci","Frank Pentangeli","Hyman Roth",];
-
-$( document ).ready(function() {           
-});
-
-function displayGifButtons()    {
-    $("#gif-buttons").empty(); 
-    for (var i = 0; i < gifFather.length; i++){
-        var gifButton = $("<button>").addClass("gif-btn");
-        //gifButton.addClass("gif-btn");
-       // gifButton.addClass("btn btn-primary")
-        //gifButton.attr("data-name", gifFather[i]);
-        gifButton.html(gifFather[i]);
-        $("#gif-buttons").append(gifButton);
+$( document ).ready(function() {
+    // An array of actions, new actions will be pushed into this array;
+    var gifFathers = ["Don Vito","Michael","Kay Adams","Sonny","Fredo","Connie","Tom Hagan","Clemenza","Tessio","Virgil Solozzo",
+              "Jack Woltz","Apollonia","Al Neri","Willy Cicci","Frank Pentangeli","Hyman Roth"];
+// Creating Functions & Methods
+    // Function that displays all gif buttons
+    function displayGifButtons(){
+        $("#gif-btns").empty(); // erasing anything in this div id so that it doesnt duplicate the results
+        for (var i = 0; i < gifFathers.length; i++){
+            var gifButton = $("<button>");
+            gifButton.addClass("gif-fathers");
+            //gifButton.addClass("btn btn-primary")
+            gifButton.attr("data-name", gifFathers[i]);
+            gifButton.text(gifFathers[i]);
+            $("#gif-btns").append(gifButton);
+        }
     }
-}
-
-displayGifButtons()
-
-//how to add buttons
-function addGifButton(){
-    $("#submit").on("click", function(){
-    var gifFather = $("#text-input").val().trim();
-    if (gifFather == ""){
-      return false; // added so user cannot add a blank button
+    // Function to add a new action button
+    function addNewButton(){
+        $("#submit").on("click", function(){
+        var gifFather = $("#text-input").val().trim();
+        if (gifFather == ""){
+          return false; // added so user cannot add a blank button
+        }
+        gifFathers.push(gifFather);
+    
+        displayGifButtons();
+        return false;
+        });
     }
-    gifFather.push(gifFather);
+    // Function to remove last action button
+        // Doesnt work properly yet removes all of the added buttons
+        // rather than just the last
+    function removeLastButton(){
+        $("remove").on("click", function(){
+        gifFathers.pop(gifFather);
+        displayGifButtons();
+        return false;
+        });
+    }
 
-    displayGifButtons();
-    return false;
+
+    ///////////////////////////////////////////////////////
+    // Function that displays all of the gifs
+    function displayGifs(){
+        var action = $(this).attr("data-name");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=dc6zaTOxFJmzC&limit=10";
+        console.log(queryURL); // displays the constructed url
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        })
+        .done(function(response) {
+            console.log(response); // console test to make sure something returns
+            $("#gifsView").empty(); // erasing anything in this div id so that it doesnt keep any from the previous click
+            var results = response.data; //shows results of gifs
+            if (results == ""){
+              alert("There isn't a gif for this selected button");
+            }
+            for (var i=0; i<results.length; i++){
+    
+                var gifDiv = $("<div>"); //div for the gifs to go inside
+                gifDiv.addClass("gifDiv");
+                // pulling rating of gif
+                var gifRating = $("<p>").text("Rating: " + results[i].rating);
+                gifDiv.append(gifRating);
+                // pulling gif
+                var gifImage = $("<img>");
+                gifImage.attr("src", results[i].images.fixed_height_small_still.url); // still image stored into src of image
+                gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); // still image
+                gifImage.attr("data-animate",results[i].images.fixed_height_small.url); // animated image
+                gifImage.attr("data-state", "still"); // set the image state
+                gifImage.addClass("image");
+                gifDiv.append(gifImage);
+                // pulling still image of gif
+                // adding div of gifs to gifsView div
+                $("#gifsView").prepend(gifDiv);
+            }
+        });
+    }
+    // Calling Functions & Methods
+    displayGifButtons(); // displays list of actions already created
+    addNewButton();
+    removeLastButton();
+    // Document Event Listeners
+    $(document).on("click", ".action", displayGifs);
+    $(document).on("click", ".image", function(){
+        var state = $(this).attr('data-state');
+        if ( state == 'still'){
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        }else{
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
     });
-}
-
+    });
 
 
 
@@ -80,11 +143,3 @@ function addGifButton(){
 //xhr.done(function(data) { 
 //    console.log(data.data[62].images.original.url); 
 //});
-
-
-
-
-
-
-
-
